b3fc653bf2333890429b416cccb94eba744daa39
kpabou1 <isidorekpabou4@gmail.com>
2024-08-15 07:26:29 +0000

    ajouts des mise à jours

--- code_Billeterie/app/Http/Controllers/FaonctionExterneController.php
+++ code_Billeterie/app/Http/Controllers/FaonctionExterneController.php
@@ -106,10 +106,6 @@ class FaonctionExterneController extends Controller
 
 
     }
-
-/*
-Route::get('/paymentconfirm/{ticketId}', [FaonctionExterneController::class, 'showPaymentConfirmPage'])->name('payment.confirm');
-Route::post('/paymentconfirm', [FaonctionExterneController::class, 'processPaymentConfirm'])->name('payment.confirm'); */
     public function showPaymentConfirmPage($ticketId, Request $request,$ticket_quantity, $orderIntentId)
     {
         
@@ -236,70 +232,6 @@ Route::post('/paymentconfirm', [FaonctionExterneController::class, 'processPayme
       return $pdf->stream('ticket.pdf');
     }
 
-    public function getAnnonceDetails($id)
-    {
-        $annonce = Annonce::find($id);
-
-        if (!$annonce) {
-            return response()->json(['message' => 'Annonce not found'], 404);
-        }
-
-        $data = [
-            'annonces' => $annonce,
-            'reponse' => 'Liste des annonces'
-        ];
-
-        return response()->json($data);
-    }
-
-
-
-    
-
-
-
-    public function showAnnonce(string $id)
-    {
-
-
-        try {
-            // Récupération de l'annonce
-            $annonce = Annonce::with(['user', 'sourcefinance', 'categoriefinance'])->findOrFail($id);
-            //recherche du name de sourcefinance
-            $sourcefinance = Sourcefinance::findOrFail($annonce->sourcesfinance_id);
-            $sourcefinance = $sourcefinance->name;
-            //si source finance id ==3
-            if ($annonce->sourcesfinance_id == 2) {
-
-                //recherche de catégorie
-                $sourcefinance = Categoriefinance::findOrFail($annonce->categorie_source_id);
-                $sourcefinance = $sourcefinance->name;
-               
-            }
-            elseif($annonce->sourcesfinance_id == 3) {
-                $sourcefinance = $annonce->autres_source;
-            }
-
-            /*
-            else{
-                $sourcefinance = $annonce->autres_source;
-            }
-                */
-          //  dd($sourcefinance);
-           
-            return view('fonction_externes.showannonce', compact('annonce','sourcefinance'));
-
-        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-            // L'annonce n'a pas été trouvée
-            return redirect()->back()->with('error', 'L\'annonce spécifiée n\'existe pas.');
-
-        } catch (Exception $e) {
-            // Autres erreurs
-            return redirect()->back()->with('error', $e->getMessage());
-        }
-        
-        
-    }
 
 
 }
